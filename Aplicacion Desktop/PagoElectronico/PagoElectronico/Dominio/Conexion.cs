867be91fefbd08a8a6866f3768456923ac1524f5
Tincho <tinchokaynar@gmail.com>
2015-05-26 14:09:27 -0300

    Agregado para ejecutar stored procedures (Tiene un error que no se como arreglar, estoy buscando)

--- Aplicacion Desktop/PagoElectronico/PagoElectronico/Dominio/Conexion.cs
+++ Aplicacion Desktop/PagoElectronico/PagoElectronico/Dominio/Conexion.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Data.SqlClient;
 using System.Windows.Forms;
+using System.Data;
+using System.Configuration;
 
 namespace PagoElectronico.Dominio
 {
@@ -12,14 +14,15 @@ namespace PagoElectronico.Dominio
         public string cadenaconexion;
         public string query = "";
         public SqlCommand command;
-        public SqlConnection cnx;
+        public static SqlConnection cnx;
         public SqlDataReader lector;
+        static string stringConexion;
 
         public Conexion()
         {
             //"Data Source=localhost\\SQLSERVER2008;Initial Catalog=LOS_METATECLA;User ID=gd;Password=gd2015";
             this.cadenaconexion = PagoElectronico.Dominio.Config.cadenaConexion();
-            this.cnx = new SqlConnection(this.cadenaconexion);
+            cnx = new SqlConnection(this.cadenaconexion);
         }
 
         public void comandear()
@@ -27,7 +30,7 @@ namespace PagoElectronico.Dominio
             command = new SqlCommand(query, cnx);
         }
 
-        public void abrirConexion()
+        public static void abrirConexion()
         {
             cnx.Open();
         }
@@ -38,7 +41,7 @@ namespace PagoElectronico.Dominio
         /*Ejecuta un query que no devuleve datos(UPDATE, INSERT, DELETE, etc)*/
         public void ejecutarNoQuery()
         {
-            this.abrirConexion();
+            abrirConexion();
             comandear();
             command.ExecuteNonQuery();
             this.cerrarConexion();
@@ -49,7 +52,7 @@ namespace PagoElectronico.Dominio
         /*SIEMPRE utilizar cerrarConexion */
         public void ejecutarQuery()
         {
-            this.abrirConexion();
+            abrirConexion();
             comandear();
             lector = command.ExecuteReader();
         }
@@ -57,6 +60,46 @@ namespace PagoElectronico.Dominio
         public bool leerReader()
         {
             return lector.Read();
+        }
+
+        //Ejecuta stored procedure con parametros y devuelve ret 
+        public static decimal ejecutarStoredProcedure(string commandtext, List<SqlParameter> ListaParametro)
+        {
+        try
+              { SqlCommand comando = new SqlCommand();
+              comando.Connection = abrirConexion(); //FIXME Realmente no se como solucionarlo, estoy buscando como se hace
+                comando.CommandText = commandtext;
+                comando.CommandType = CommandType.StoredProcedure;
+
+                foreach (SqlParameter elemento in ListaParametro)
+                {
+                    comando.Parameters.Add(elemento);
+                }
+
+                comando.ExecuteNonQuery();
+                return (decimal)comando.Parameters["@ret"].Value;
+            }
+            catch (SqlException e)
+            {
+                MessageBox.Show("" + e.Message, "Error");
+                return 0;
+            }
+        }
+        //Ejecuta Stored Procedure sin Retorno
+        public static void ejecutarStoredProcedureSinRet(string commandtext, List<SqlParameter> ListaParametro)
+        {
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = abrirConexion(); //FIXME los mismo que antes
+            comando.CommandText = commandtext;
+            comando.CommandType = CommandType.StoredProcedure;
+
+            foreach (SqlParameter elemento in ListaParametro)
+            {
+                comando.Parameters.Add(elemento);
+            }
+
+            comando.ExecuteNonQuery();
+
         }
 
     }
