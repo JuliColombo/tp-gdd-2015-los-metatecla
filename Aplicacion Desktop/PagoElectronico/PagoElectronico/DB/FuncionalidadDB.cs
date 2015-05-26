8167631a419d5e67973b8ae970d977bd10b3cc28
Tincho <tinchokaynar@gmail.com>
2015-05-26 14:15:41 -0300

    Me faltaron estas dos clases

---
+++ Aplicacion Desktop/PagoElectronico/PagoElectronico/DB/FuncionalidadDB.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PagoElectronico.Dominio;
+using System.Data.SqlClient;
+
+namespace PagoElectronico.DB
+{
+    class FuncionalidadDB
+    {
+        public static List<Funcionalidad> obtenerFuncionalidades()
+        {
+            List<Funcionalidad> funcionalidades = new List<Funcionalidad>();
+            Conexion conexion = new Conexion();
+            conexion.query = string.Format("SELECT * FROM LOS_METATECLA.Funcionalidad");
+            conexion.ejecutarQuery();
+            if (conexion.lector.HasRows)
+            {
+                while (conexion.leerReader())
+                {
+                    Funcionalidad func = new Funcionalidad((int)(decimal)conexion.lector["Id_Funcionalidad"], (string)conexion.lector["Nombre"]);
+                    funcionalidades.Add(func);
+                }
+            }
+            conexion.cerrarConexion();
+            return funcionalidades;
+        }
+
+
+        public void AgregarFuncionalidadDelRol(int idRol,Funcionalidad funcionalidad){
+            List<SqlParameter> ListParam = new List<SqlParameter>();
+            ListParam.Add(new SqlParameter("@id_rol",idRol));
+            ListParam.Add(new SqlParameter("@funcionalidad", funcionalidad.nombre));
+            Conexion.ejecutarStoredProcedureSinRet("MERCADONEGRO.AgregarFuncionalidadARol", ListParam);
+        
+        }
+    }
+}
---
+++ Aplicacion Desktop/PagoElectronico/PagoElectronico/DB/RolDB.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PagoElectronico.Dominio;
+using System.Data.SqlClient;
+
+namespace PagoElectronico.DB
+{
+    class RolDB
+    {
+         public static List<Rol> obtenerRoles()
+        {
+            List<Rol> roles = new List<Rol>();
+            Conexion conexion = new Conexion();
+            conexion.query = string.Format("SELECT * FROM LOS_METATECLA.Rol");
+            conexion.ejecutarQuery();
+            if (conexion.lector.HasRows)
+            {
+                while (conexion.leerReader())
+                {
+                    Rol rol = new Rol((int)(decimal)conexion.lector["Id_Rol"], (string)conexion.lector["Nombre"], (bool)conexion.lector["Habilitado"]);
+                    roles.Add(rol);
+                }
+            }
+            conexion.cerrarConexion();
+            return roles;
+        }
+    
+        public int AgregarRol(string nombre,bool estado,List<Funcionalidad> funcionalidadesRol){ //No se si esta bien hecho el tema de ejecutarStoredProcedure
+            List<SqlParameter> ListParam = new List<SqlParameter>();
+            int respuesta = -1;
+            //1. Primero agrego a la tabla de roles
+            ListParam.Add(new SqlParameter("@nombre", nombre));
+            ListParam.Add(new SqlParameter("@estado", estado));
+            respuesta = (int)Conexion.ejecutarStoredProcedure("MERCADONEGRO.agregarRolNuevo", ListParam);
+            //2. Verifico que se haya agregado
+            if(respuesta != -1) {
+            //3. Agrego a Funcionalidades_Rol todas las funcionalidades de este rol
+                FuncionalidadDB DBFuncionalidad = new FuncionalidadDB();
+                foreach (Funcionalidad funcionalidad in funcionalidadesRol)
+                    {
+                        DBFuncionalidad.AgregarFuncionalidadDelRol(respuesta, funcionalidad);
+                    } return 1;
+            }
+            return 0;
+        }
+
+    
+    
+    }
+}
