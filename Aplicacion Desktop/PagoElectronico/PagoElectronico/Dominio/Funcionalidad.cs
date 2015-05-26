bb13b366f169f2d7f11a45cbd062bd41af3d38b9
Tincho <tinchokaynar@gmail.com>
2015-05-26 14:10:29 -0300

    Agregadas clases que necesito para el manejo de roles y funcionalidades

---
+++ Aplicacion Desktop/PagoElectronico/PagoElectronico/Dominio/Funcionalidad.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PagoElectronico.Dominio
+{
+    class Funcionalidad
+    {
+        public int id{ get; set; }
+        public string nombre { get; set; }
+
+        public Funcionalidad(int id, string nombre)
+        {
+            this.id = id;
+            this.nombre = nombre;
+
+        }
+    }
+}
--- Aplicacion Desktop/PagoElectronico/PagoElectronico/Dominio/Rol.cs
+++ Aplicacion Desktop/PagoElectronico/PagoElectronico/Dominio/Rol.cs
@@ -7,5 +7,16 @@ namespace PagoElectronico.Dominio
 {
     class Rol
     {
+          public int id { get; set; }
+          public string nombre { get; set; }
+          public bool habilitado { get; set; }
+          public List<Funcionalidad> funcionalidades = new List<Funcionalidad>();
+
+        public Rol(int id, string nombre, bool habilitado)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.habilitado = habilitado;
+        }
     }
 }
