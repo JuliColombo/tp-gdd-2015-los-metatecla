5abbc7f8fbc9b12291564a2469f4e39aff88e422
Juli Colombo <julietanataliacolombo@yahoo.com.ar>
2015-05-27 21:22:22 -0300

    bastante cocinado el tema de crear una cuenta

--- Aplicacion Desktop/PagoElectronico/PagoElectronico/ABM Cuenta/CrearCuentaForm.cs
+++ Aplicacion Desktop/PagoElectronico/PagoElectronico/ABM Cuenta/CrearCuentaForm.cs
@@ -18,9 +18,45 @@ namespace PagoElectronico.ABM_Cuenta
             DB.TipoCuentaDB.cargarTipos(comboBox_tipo.Items);
         }
 
-        private void CrearCuentaForm_Load(object sender, EventArgs e)
-        {
 
+        private void btn_confirmar_Click(object sender, EventArgs e)
+        {
+            if (validaciones())
+            {
+                crearCuenta(textBox_nro_cuenta.Text, textBox_usuario.Text, comboBoxPais.Text, comboBox_tipo.Text);
+            }
+            else
+            {
+                ventanaDeError("No se pudo crear cuenta");
+            }
+        }
+
+        private void crearCuenta(string nro_cuenta, string usuario, string pais, string tipo)
+        {
+            PagoElectronico.DB.CuentaDB.crearCuenta(nro_cuenta, usuario, pais, tipo); //falta estado, fecha y moneda TODO 
+        }
+
+
+        private void ventanaDeError(String error)
+        {
+            Form f = new PagoElectronico.Login.ErrorForm(error);
+            f.ShowDialog();
+        }
+
+        private bool validaciones()
+        {
+            return validarUsuario(textBox_usuario.Text) && validadNroCuenta(textBox_nro_cuenta.Text);
+            //TODO validarFechaApertura(textBox_fecha.Text);
+        }
+
+        private bool validadNroCuenta(string nro_cuenta)
+        {
+            return PagoElectronico.DB.CuentaDB.noExisteCuenta(nro_cuenta); 
+        }
+
+        private bool validarUsuario(string usuario)
+        {
+            return PagoElectronico.DB.UsuarioDB.existe(usuario); 
         }
 
 
--- Aplicacion Desktop/PagoElectronico/PagoElectronico/DB/CuentaDB.cs
+++ Aplicacion Desktop/PagoElectronico/PagoElectronico/DB/CuentaDB.cs
@@ -7,5 +7,32 @@ namespace PagoElectronico.DB
 {
     class CuentaDB
     {
+        internal static bool noExisteCuenta(string nro_cuenta)
+        {
+            double cuenta = Convert.ToDouble(nro_cuenta);
+
+            PagoElectronico.Dominio.Conexion conexion = new PagoElectronico.Dominio.Conexion();
+            conexion.query = string.Format(
+                "SELECT * FROM LOS_METATECLA.Cuenta WHERE Cuenta_Numero = {0}", cuenta);
+            conexion.ejecutarQuery();
+            bool valida = !(conexion.leerReader());
+            conexion.cerrarConexion();
+            return valida;
+        }
+
+        internal static void crearCuenta(string nro_cuenta, string usuario, string pais, string tipo)
+        {
+            double cuenta = Convert.ToDouble(nro_cuenta);
+            double pais_id = PagoElectronico.DB.PaisDB.getID(pais);
+            int tipo_id = PagoElectronico.DB.TipoCuentaDB.getId(tipo);
+            double user_id = PagoElectronico.DB.UsuarioDB.getId(usuario);
+
+            //TODO: FALTA MONEDA Y FECHA
+            PagoElectronico.Dominio.Conexion conexion = new PagoElectronico.Dominio.Conexion();
+            conexion.query = string.Format(
+                "INSERT INTO LOS_METATECLA.Cuenta (Cuenta_Numero, Cuenta_Fecha_Creacion, Cuenta_Estado, Cuenta_Pais_Codigo, Cuenta_Fecha_Cierre, Cuenta_Cliente, Cuenta_Tipo) VALUES({0}, NULL, NULL, {1}, NULL, {2}, {3})", cuenta, pais_id, user_id, tipo_id);
+            conexion.ejecutarNoQuery();
+
+        }
     }
 }
