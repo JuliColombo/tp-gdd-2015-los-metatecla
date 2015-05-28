9416e6f86d7b76235a82c98f1cb4fd410ba0de85
Tincho <tinchokaynar@gmail.com>
2015-05-27 21:34:54 -0300

    Me falto subir esta modificacion

--- Aplicacion Desktop/PagoElectronico/PagoElectronico/Dominio/Rol.cs
+++ Aplicacion Desktop/PagoElectronico/PagoElectronico/Dominio/Rol.cs
@@ -10,6 +10,7 @@ namespace PagoElectronico.Dominio
           public int id { get; set; }
           public string nombre { get; set; }
           public bool habilitado { get; set; }
+          public string estado { get; set; }
           public List<Funcionalidad> funcionalidades = new List<Funcionalidad>();
 
         public Rol(int id, string nombre, bool habilitado)
@@ -17,6 +18,13 @@ namespace PagoElectronico.Dominio
             this.id = id;
             this.nombre = nombre;
             this.habilitado = habilitado;
+            if (habilitado) //Es muy turbio pero la es la unica forma de sacar el checkbox del datagrid que encontre
+            {
+                estado = "Habilitado";
+            }
+            else {
+                estado = "Inhabilidato";
+            }
         }
     }
 }
